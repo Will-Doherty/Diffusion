00cdfe22ed8927ad48aa3007ba23cd39c58aa7f0
Will Doherty <wdoherty8@gmail.com>
2025-09-26 00:04:06 +0100

    eod commit - partway through sliced_score_matching_gm.py

--- sliced_score_matching_gm.py
+++ sliced_score_matching_gm.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.nn.functional import relu, mse_loss
+from torch.nn.functional import gelu, mse_loss
 from torch.func import hessian
 from dataclasses import dataclass
 import matplotlib
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import os
 import subprocess
 from tqdm import tqdm
+import argparse
+from pathlib import Path
 
 
 # --- DATASET GENERATION ---
@@ -62,12 +64,13 @@ class GaussianMixture:
 
 # --- TRAINING CODE ---
 
-def calculate_sm_objective(model_fwd_pass_fn, x):
-    print(hessian(model_fwd_pass_fn)(x).sum())
-    if 
+def calculate_sm_objective(model, x):
+    x = x.squeeze(0)
+    def scalar_fn(inp):
+        return model(inp).sum()
     loss = (
-        torch.trace(hessian(model_fwd_pass_fn)(x).sum())
-        # + 0.5 * torch.norm(model.parameters())
+        torch.trace(hessian(scalar_fn)(x))
+        + 0.5 * torch.linalg.vector_norm(torch.cat([p.reshape(-1) for p in model.parameters()]))
     )
     return loss
 
@@ -90,7 +93,7 @@ def calculate_sm_objective(model_fwd_pass_fn, x):
 class ScoreFnNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer1 = nn.Linear(1, 128)
+        self.layer1 = nn.Linear(2, 128)
         self.layer2 = nn.Linear(128, 128)
         self.layer3 = nn.Linear(128, 1, bias=False)
 
@@ -100,30 +103,37 @@ class ScoreFnNet(nn.Module):
 
     def forward(self, x):
         # TODO - condition on time
-        x = relu(self.layer1(x))
-        x = relu(self.layer2(x))
+        x = gelu(self.layer1(x))  # swapped to gelu because relu has 0s everywhere in its Hessian
+        x = gelu(self.layer2(x))
         x = self.layer3(x)
         return x
 
 @dataclass
-class Config:
+class TrainingConfig:
     lr = 1e-4
-    n = 30_000
+    n = 1_000
 
-def train_score_matching():
-    cfg = Config()
+@dataclass
+class InferenceConfig:
+    n_steps = 10
+    initial_x = (0, 0)
+    step_size = 0.0001
+
+def train_score_matching(cfg: TrainingConfig):
     model = ScoreFnNet()
     optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
-    gm = GaussianMixture(0.0, 3.0, 1.0, 1.0, 0.5)
+    gm = GaussianMixture((0.0, 0.0), (3.0, 3.0), (1.0, 1.0), (1.0, 1.0), 0.5)
     losses = []
     print("Training...")
-    for i in tqdm(range(cfg.n)):
+    for i in range(cfg.n):
         x = gm.sample(n=1).to(torch.float32)
         model.zero_grad(set_to_none=True)
-        loss = calculate_sm_objective(model.forward, x)
+        loss = calculate_sm_objective(model, x)
         losses.append(loss.item())
         loss.backward()
         optimizer.step()
+        if i % 100 == 0 and i > 0:
+            print(f"step {i}, loss: {sum(losses[-100:]) / 100:.6f}")
     return model
 
 
@@ -133,7 +143,8 @@ def langevin_step(current_x: torch.Tensor, step_size: float, gm: GaussianMixture
     noise = torch.randn((), dtype=torch.float64)
     return current_x + 0.5 * step_size * model(current_x) + torch.sqrt(torch.tensor(step_size, dtype=torch.float64)) * noise
 
-def run_langevin_sampling(n_steps: int, initial_x: float, step_size: float, gm: GaussianMixture) -> torch.Tensor:
+def run_langevin_sampling(cfg: InferenceConfig) -> torch.Tensor:
+    n_steps, initial_x, step_size = cfg.n_steps, cfg.initial_x, cfg.step_size
     samples = torch.zeros(n_steps, dtype=torch.float64)
     current_x = torch.tensor(initial_x, dtype=torch.float64)
     samples[0] = current_x
@@ -144,6 +155,24 @@ def run_langevin_sampling(n_steps: int, initial_x: float, step_size: float, gm: 
 
 
 if __name__ == "__main__":
-    trained_model = train_score_matching()
-    run_langevin_sampling()
+    weight_directory = Path("model_weights")
+    weight_path = weight_directory / "sliced_score_matching_gm_weights.pt"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inference_only", action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()
+
+    if args.inference_only:
+        if not weight_path.exists():
+            raise FileNotFoundError("Existing weight path not found. You might need to train the model first!")
+        trained_model = torch.load(weight_path)
+    else:
+        training_cfg = TrainingConfig()
+        trained_model = train_score_matching(training_cfg)
+        weight_directory.mkdir(exist_ok=True)
+        torch.save(trained_model, weight_path)
+
+    inference_cfg = InferenceConfig()
+    inference_samples = run_langevin_sampling(inference_cfg)
+    
 
